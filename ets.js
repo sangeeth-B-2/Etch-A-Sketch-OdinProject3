f4369f72f6c6529862887790c9ed88614d38889e
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-17 13:42:33 +0530

    changing color finally works

--- ets.js
+++ ets.js
@@ -1,7 +1,10 @@
 let parentDiv = document.getElementById('container');
 let inputSumit = document.getElementById('sumit');
-let inputBox = (document.getElementById('inputBox'));
+let inputBox = document.getElementById('inputBox');
 let favColor = document.getElementById('favColor');
+let resetBtn = document.getElementById('resetBtn');
+
+
 
 let colorName;
 
@@ -14,9 +17,8 @@ favColor.addEventListener('input', () => {
 
 inputSumit.addEventListener('click', () => {
     let n = inputBox.value;
-
     parentDiv.innerHTML = "";
-    let childSize = (parentDiv.clientWidth) / n;
+    let childSize = (parentDiv.clientWidth / n);
     console.log(childSize);
     //looping for grid
     for (let j = 1; j <= n; j++) {
@@ -27,20 +29,25 @@ inputSumit.addEventListener('click', () => {
             childDiv.style.width = childSize + 'px';
             childDiv.style.height = childSize + 'px';
             parentDiv.appendChild(childDiv);
-        
+
+            let childGrid = parentDiv.children;
+
+            Array.from(childGrid).forEach(child => {
+                child.addEventListener('click', (event) => {
+                    if (colorName) {
+
+                        event.target.style.backgroundColor = colorName;
+                    }
+                });
+            });
 
         }
     }
 });
 
 
-let childGrid = parentDiv.children;
 
-Array.from(childGrid).forEach(child => {
-    child.addEventListener('click', (event) => {
-        if (colorName) {
-
-            event.target.style.backgroundColor = colorName;
-        }
-    });
+resetBtn.addEventListener('click', () => {
+    parentDiv.innerHTML = '';
+    inputBox.value = '';
 });
